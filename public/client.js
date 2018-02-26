1dab7fc94ca1fc1680c1bdea64ebc94e2de4b5b1
Matt McGraw <mattymcgraw@me.com>
2018-02-25 22:15:59 -0800

    Added functionality

--- public/client.js
+++ public/client.js
@@ -1,22 +1,28 @@
 TrelloPowerUp.initialize({
-  'board-buttons': function(t, options){
-    return [{
-      text: 'Final Project',
-      url: 'https://trello.com/b/wD3Gigzp',
-      target: 'Final Project'
-    }];
-  },
-  'format-url': function (t, options) {
-    // options.url has the url that we are being asked to format
-    // in our response we can include an icon as well as the replacement text
+	'card-buttons': function(t, options){
+		return [{
+			icon: 'https://github.com/mattymcgraw/final-project/blob/master/public/sanctum-sanctorum.png',
+			text: 'Estimate Time',
+    	}];
+	},
+	'board-buttons': function(t, options){
+		return [{
+			text: 'Final Project',
+			url: 'https://trello.com/b/wD3Gigzp',
+			target: 'Final Project'
+	}];
+	},
+	'format-url': function (t, options) {
+		// options.url has the url that we are being asked to format
+		// in our response we can include an icon as well as the replacement text
 
-    return {
-      icon: GRAY_ICON, // don't use a colored icon here
-      text: '👉 ' + options.url + ' 👈' 
-    };
+		return {
+			icon: GRAY_ICON, // don't use a colored icon here
+			text: '👉 ' + options.url + ' 👈' 
+		};
 
-    // if we don't actually have any valuable information about the url
-    // we can let Trello know like so:
-    throw t.NotHandled();
-  }
+		// if we don't actually have any valuable information about the url
+		// we can let Trello know like so:
+		throw t.NotHandled();
+	}
 });
